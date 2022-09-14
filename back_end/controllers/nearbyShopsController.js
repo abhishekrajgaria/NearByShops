814b73aeb4644685e9f58e141e3b25250ba44f1c
abhrajga <abhishek.rajgaria@publicissapient.com>
2022-09-15 00:18:18 +0530

    code refactor and add error handling

--- back_end/controllers/nearbyShopsController.js
+++ back_end/controllers/nearbyShopsController.js
@@ -1,11 +1,6 @@
 const axios = require("axios");
 
-exports.getShops = async (req, res, next) => {
-  console.log(req.body);
-  const postcode = req.body.postcode;
-  console.log(postcode);
-  // postcode.replace(/ /g, "").toLowerCase();
-  // console.log(postcode);
+const getLocation = async function (postcode) {
   const options = {
     method: "GET",
     url:
@@ -13,34 +8,65 @@ exports.getShops = async (req, res, next) => {
       postcode +
       "?api-key=eBK94-gxfUGcDN3okrAeOw36662",
   };
-
-  const nearbyshops = {
-    method: "GET",
-    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-  };
   try {
     const response = await axios.request(options);
-    // console.log(response);
-    const data = response.data;
-    const latitude = data.latitude;
-    const longitude = data.longitude;
-    console.log(latitude + "," + longitude);
-    const locations = latitude + "," + longitude;
-    const nearbyShopsResponse = await axios.get(
-      "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-      {
-        params: {
-          location: locations,
-          radius: 1500,
-          keyword: "shops",
-          key: "AIzaSyABb4Hz-BYlwW1ULLR-1_jmiv27r8EjxG8",
-        },
-      }
-    );
-    const nearbyShopsData = nearbyShopsResponse.data;
-    // console.log(nearbyShopsData);
-    res.status(200).json(nearbyShopsData);
-  } catch (error) {
-    console.log(error);
+    if (response.status === 200) {
+      const data = response.data;
+      const location = data.latitude + "," + data.longitude;
+      return location;
+    }
+  } catch (err) {
+    throw err.response.data;
+  }
+};
+
+const getPlaces = async function (locations, placeType, placeWithInRadius) {
+  let next_page_token;
+  const nearbyPlaces = {
+    method: "GET",
+    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+    params: {
+      location: locations,
+      radius: placeWithInRadius ? placeWithInRadius : 1000,
+      keyword: placeType ? placeType : "store",
+      key: "AIzaSyABb4Hz-BYlwW1ULLR-1_jmiv27r8EjxG8",
+      pagetoken: next_page_token,
+    },
+  };
+  let nearbyPlacesResponse;
+  try {
+    nearbyPlacesResponse = await axios.request(nearbyPlaces);
+    return {
+      next_page_token: nearbyPlacesResponse.data.next_page_token
+        ? nearbyPlacesResponse.data.next_page_token
+        : "",
+      nearby_places: nearbyPlacesResponse.data.results,
+      status: nearbyPlacesResponse.data.status,
+    };
+  } catch (err) {
+    throw err.response.data;
+  }
+};
+
+exports.getShops = async (req, res, next) => {
+  try {
+    if (req.body.postcode) {
+      const postcode = req.body.postcode;
+      const placeType = req.body.type;
+      const radius = req.body.radius
+      console.log(postcode);
+
+      const location = await getLocation(postcode);
+      console.log("Location", location);
+      const nearbyPlaces = await getPlaces(location, placeType, radius);
+      res.status(200).json(nearbyPlaces);
+    }
+    else{
+        throw `please provide "postcode" in body`;
+    }
+  } catch (err) {
+    res.status(400).json({
+      "error_message": err,
+    });
   }
 };
